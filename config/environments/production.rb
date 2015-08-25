ae939667a7530e1e97ec1b6c30df759cca3ec54c
Irakli Koridze <iraklikori@gmail.com>
2015-08-25 12:22:33 +0400

    transfer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,15 +84,16 @@ Rails.application.configure do
 	#required for heroku
 	config.action_mailer.default_url_options = { :host => 'artiveria.com'}
 
-config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
 
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.mandrillapp.com",
-  :port                 => 587,
-  :domain               => "artiveria.com",
-  :user_name            => "iraklikori@gmail.com",
-  :password             => "jATKRtyfdKKoke3kjVrgaA",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :domain               => "artiveria.com",
+    :user_name            => "iraklikori@gmail.com",
+    :password             => "jATKRtyfdKKoke3kjVrgaA",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['MIN_THREADS']  || 4), Integer(ENV['MAX_THREADS'] || 16)
 
+preload_app!
+
 port 3000
 environment 'production'
 
